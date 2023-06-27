93a0846d306fd6ebcaa885a966380f05e4be9def
hanswong <hanswong@ctil.com>
2023-06-27 12:16:12 +0800

    init version

---
+++ DapperDbHandler/DbHandler.cs
@@ -0,0 +1,46 @@
+﻿using Dapper;
+using System.Collections.Immutable;
+using System.Data.SqlClient;
+
+namespace DapperDbHandler
+{
+    public class DbHandler
+    {
+        public string ConnectionString { get; }
+        public DbHandler(string connectionString)
+        {
+            ConnectionString = connectionString;
+        }
+        public async Task<ImmutableList<T>> RetrieveDataAsync<T>(string sql, object? param = null) where T : class
+        {
+            // Create connection to SQL Server
+            using SqlConnection connection = new(ConnectionString);
+            // Open connection
+            await connection.OpenAsync();
+            return (await connection.QueryAsync<T>(sql, param)).ToImmutableList();
+        }
+        public async Task ExecuteSqlWithTx(ImmutableList<SqlCommandContainer> sqlCommands)
+        {
+            using var connection = new SqlConnection(ConnectionString);
+            await connection.OpenAsync();
+            using var transaction = await connection.BeginTransactionAsync();
+            try
+            {
+                // Execute SQL statements using Dapper  
+                foreach (var sqlCommand in sqlCommands)
+                {
+                    await connection.ExecuteAsync(sqlCommand.Sql, sqlCommand.Parameters, transaction);
+                }
+
+                // Commit the transaction  
+                await transaction.CommitAsync();
+            }
+            catch
+            {
+                // Rollback the transaction if an error occurs  
+                await transaction.RollbackAsync();
+                throw;
+            }
+        }
+    }
+}
---
+++ DapperDbHandler/SqlCommandContainer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DapperDbHandler
+{
+    public class SqlCommandContainer
+    {
+        public string Sql { get; }
+        public object? Parameters { get; }
+        public SqlCommandContainer(string sql, object? parameters = null)
+        {
+            Sql = sql;
+            Parameters = parameters;
+        }
+    }
+}
